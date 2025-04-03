a5f1db0a1b8f05fef1bcf0879c594796d6a492e3
samaralves1 <samaramaria0220@gmail.com>
2025-04-02 22:02:11 -0300

    fix: alterando o estilo do forms

--- src/components/MomentForms.jsx
+++ src/components/MomentForms.jsx
@@ -1,35 +1,38 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 import { supabase } from "./../services/supabaseClient";
-import { formatDate } from './../utils/formatDate';
-import "./styles.css"
-
+import { formatDate } from "./../utils/formatDate";
+import "./styles.css";
 
 export default function MomentForms() {
-    const [generalData, setGeneralData] = useState([]);
-    
-      const fetchGeneral = async () => {
-        const { data, error } = await supabase.from('General').select('*');
-        if (error) {
-          console.error('Erro ao buscar dados:', error);
-        } else {
-          setGeneralData(data || []);
-        }
-      };
-      
-      useEffect(() => {
-        fetchGeneral();
-      }, []);
+  const [generalData, setGeneralData] = useState([]);
+
+  const fetchGeneral = async () => {
+    const { data, error } = await supabase.from("General").select("*");
+    if (error) {
+      console.error("Erro ao buscar dados:", error);
+    } else {
+      setGeneralData(data || []);
+    }
+  };
+
+  useEffect(() => {
+    fetchGeneral();
+  }, []);
   return (
-    <div className='containerGeral'>
-      <h1 className='title'>Forms do momento</h1>
+    <div className="containerGeral">
+      <h1 className="title">Forms do momento</h1>
       <ul className="containerMomentForms">
         {generalData.map((item) => (
           <li>
-            <span className='Name'>{item.name}</span> {" "}
-            <a href={item.link} target="_blank" rel="noopener noreferrer">
-              Link
-            </a>{" "}
-            <span className='Date'>{formatDate(item.date)}</span>
+            <a
+              className="Name"
+              href={item.link}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              {item.name}
+            </a>
+            <span className="Date">{formatDate(item.date)}</span>
           </li>
         ))}
       </ul>
