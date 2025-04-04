c68761d6f71b1a86d0e5f0b78e8b545ad592852a
felixlaryssa <felixllaryssa@gmail.com>
2025-04-04 03:21:17 -0300

    fix: alterações

---
+++ src/components/WelcomeBanner/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './WelcomeBanner.css';
+
+const WelcomeBanner = () => {
+    return (
+        <div className="welcome-banner">
+        <h1 className="welcome-title">Seja muito Bem-vindo ao Portal Orc !</h1>
+        </div>
+    );
+};
+
+export default WelcomeBanner;
